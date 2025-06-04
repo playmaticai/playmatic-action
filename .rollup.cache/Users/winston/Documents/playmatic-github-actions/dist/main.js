1bc00b689897b019594c5f95fffe3f003ad153cf
ElasticBottle <winstonyeo99@yahoo.com>
2025-06-05 04:13:38 +0800

    chore: update build

--- .rollup.cache/Users/winston/Documents/playmatic-github-actions/dist/main.js
+++ .rollup.cache/Users/winston/Documents/playmatic-github-actions/dist/main.js
@@ -42,7 +42,7 @@ export async function run() {
             body.pullRequestNumber = pullRequestNumber;
         if (ref)
             body.ref = ref;
-        core.debug(`Request body: ${JSON.stringify(body)}`);
+        core.debug(`Request Body: ${JSON.stringify(body)}`);
         const response = await fetch(PLAYMATIC_API_ENDPOINT, {
             method: "POST",
             headers: {
