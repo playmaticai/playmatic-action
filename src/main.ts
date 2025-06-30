a199f0f8f41134343f01071af673bd4149837eab
Edward Sun <eedwardsun@gmail.com>
2025-06-29 17:12:07 -0700

    Add selection for run-exploratory-test

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ export async function run(): Promise<void> {
     const environmentId: string | undefined =
       core.getInput("environment-id") || undefined;
     const runAllSavedTests = core.getBooleanInput("run-all-saved-tests");
-
+    const runExploratoryTest = core.getBooleanInput("run-exploratory-test");
+    
     if (!testUrl && !environmentId) {
       core.setFailed("Either 'test-url' or 'environment-id' must be provided.");
       return;
@@ -54,6 +55,7 @@ export async function run(): Promise<void> {
     if (pullRequestNumber) body.pullRequestNumber = pullRequestNumber;
     if (ref) body.ref = ref;
     body.runAllSavedTests = runAllSavedTests;
+    body.runExploratoryTest = runExploratoryTest;
 
     core.debug(`Request Body: ${JSON.stringify(body)}`);
 
