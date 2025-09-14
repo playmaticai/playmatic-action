480fcd0b0422457a987a0444fd9d4fd1f1feb649
Edward Sun <eedwardsun@gmail.com>
2025-09-13 21:44:46 -0700

    Revert making the test fail on purpose

--- playmatic-tests/sample.test.ts
+++ playmatic-tests/sample.test.ts
@@ -3,8 +3,8 @@ import { test, testStep } from "@playmatic/sdk";
 test("Playmatic Waitlist Calendar Loads", () => {
   testStep("Navigate to Playmatic.ai", async ({ page }) => {
     // The cached test code will pass, and computer use will not be needed
-    await page.goto("https://playmatic2222.ai");
-  }, { cacheOnly: true});
+    await page.goto("https://playmatic.ai");
+  });
 
   testStep("Click the button to join the waitlist", async ({ page }) => {
     // The cached test code here will fail and fall back to computer use
